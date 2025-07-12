75d7d4c08d35e59f0e8e95074d53a379d4eb19de
Jonah Taylor <jonahbtaylor05@gmail.com>
2025-07-12 16:35:34 -0400

    Removed tmux bug, modified swaylock color, allowed focus switching in resize mode

--- tmux/tmux-start.sh
+++ tmux/tmux-start.sh
@@ -19,8 +19,3 @@ tmux select-window -t "$SESSION":1
 # Attach to the session
 tmux -2 attach-session -t "$SESSION"
 
-# Controls
-bind -r h resize-pane -L 5
-bind -r j resize-pane -D 5
-bind -r k resize-pane -U 5
-bind -r l resize-pane -R 5
