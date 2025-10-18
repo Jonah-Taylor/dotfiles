2ed735171c9eec3af789b2c31848c37b1dde1f1a
Jonah Taylor <jonahbtaylor05@gmail.com>
2025-10-18 00:35:02 -0400

    Nvim now changes buffers by typing your number and hitting enter in nm mode

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,3 +7,13 @@ vim.keymap.set('n', '\\h', '<cmd>nohl<cr>', { desc = "Clear search highlights" }
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration' })
 vim.keymap.set('n', '<leader>r', ':LspRestart<CR>', { desc = 'Restart LSP' })     
+
+-- Type a number in normal mode, then press <CR> to jump to that buffer
+vim.keymap.set('n', '<CR>', function()
+  local count = vim.v.count
+  if count > 0 then
+    vim.cmd('buffer ' .. count)
+  else
+    vim.cmd('normal! <CR>')
+  end
+end, { noremap = true, silent = true })
